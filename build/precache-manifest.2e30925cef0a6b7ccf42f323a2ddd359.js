103c67157d7913d38ef6f56bb73a1052d295fcdf
DanTayar <dantayar28@gmail.com>
2019-06-27 14:38:16 +0300

    started server

---
+++ build/precache-manifest.2e30925cef0a6b7ccf42f323a2ddd359.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a7cc1e02985ca3dc523a83eaa1dbff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21b1bae98b0a9f37a545",
+    "url": "/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "17b64f88a862ccdc0597",
+    "url": "/static/css/main.c6f7ef53.chunk.css"
+  },
+  {
+    "revision": "21b1bae98b0a9f37a545",
+    "url": "/static/js/2.7fec4517.chunk.js"
+  },
+  {
+    "revision": "17b64f88a862ccdc0597",
+    "url": "/static/js/main.c1f3096b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a2018c34d7e9616fe4548c7a4b334f6",
+    "url": "/static/media/goldRecord.6a2018c3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+
+app.use( express.static('build') );
+app.use( express.json() );
+
+let submissions =[]
+
+app.post('/submit' , (req , res) =>{
+	submissions.push(req.body);
+	res.status(201).send('submitted su ccesfuly');
+});
+
+app.get('/submissions', (req, res) =>{
+	res.json(submissions);
+
+
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
